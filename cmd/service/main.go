ebf5a56e1af6d5be552be08542951d00bc5aa90f
Vladislav Vakhrushev <vladislav.vakhrushev@tabby.ai>
2024-02-21 10:47:10 +0400

    Prepare-1 clean

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/Vafelence/fifa_project/internal/pkg/server"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Vafelence/fifa_project/internal/pkg/server"
+)
 
 func main() {
-	server.New()
+	srv := server.New()
+	fmt.Println(srv.Info())
+	err := srv.Run()
+	if err != nil {
+		fmt.Println("can't start server", err)
+		os.Exit(1)
+	}
 }
