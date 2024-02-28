534a30f3a3fd949d72299450b442864b2882222e
Vladislav <postformy@mail.ru>
2024-02-28 12:33:50 +0400

    Prepare 2 (#2)
    * Add gracefull shutdown
    
    * Add template and main page
    
    ---------
    
    Co-authored-by: Vladislav Vakhrushev <vladislav.vakhrushev@tabby.ai>

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/Vafelence/fifa_project/internal/pkg/server"
 )
 
 func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "app", "fifa-service")
 	srv := server.New()
-	fmt.Println(srv.Info())
-	err := srv.Run()
+	router := srv.Router()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "main.tmpl", &gin.H{
+			"page_title":    "Выбор игровых команд",
+			"random_team_1": "Одутловатые",
+			"random_team_2": "Отрафиты",
+		})
+	})
+
+	err := srv.Run(ctx)
 	if err != nil {
 		fmt.Println("can't start server", err)
 		os.Exit(1)
